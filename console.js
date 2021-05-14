116724c1a36afec4d641dd984f512d4513eeb9cf
Anush <33168386+AnushK-Fro@users.noreply.github.com>
2021-05-13 21:25:48 -0400

    Create console.js

---
+++ console.js
@@ -0,0 +1,15 @@
+fetch("https://discord.com/api/v8/channels/Channel ID Here/invites:, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        max_age: 86400,
+                        max_uses: 0,
+                        target_application_id: "755827207812677713",  // Youtube Together: 755600276941176913 | Poker Night: 755827207812677713 | Betrayal.io: 773336526917861400 | Fishington.io: 814288819477020702 
+                        target_type: 2,
+                        temporary: false,
+                        validate: null
+                    }),
+                    headers: {
+                        "Authorization": "Discord Token Here",
+                        "Content-Type": "application/json"
+                    }
+                })
